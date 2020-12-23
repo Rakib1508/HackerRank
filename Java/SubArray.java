e739dc2c07e3f1d9027aad05ea6547bb0230d61e
Rakib1508 <abdur.rakib.1508@gmail.com>
2020-12-23 16:53:32 +0600

    2020.12.23

---
+++ Java/SubArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SubArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int negCnt = 0;
+        for (int i=0; i < n; i++) {
+            arr[i] = sc.nextInt(); 
+            int j = i;
+            int sum = 0;
+            while (j>=0) {
+                sum += arr[j];
+                if(sum < 0)
+                    negCnt++;
+                j--;
+            }
+         } 
+        System.out.println(negCnt); 
+    }
+}
